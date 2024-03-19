ad54cea81e81a26c8718a0886df141e60758463b
Ben Owen <ben@bensuniverse.com>
2024-03-18 22:14:34 -0400

    Packet

--- src/packet.h
+++ src/packet.h
@@ -1,6 +1,28 @@
 #ifndef __PACKET_H__
 #define __PACKET_H__
 
+#include "stdint.h"
 
+// top-level struct for a packet
+typedef struct Packet
+{
+	uint8_t id;
+	uint32_t time;
+	
+	uint8_t src;
+	uint8_t dst;
+	DataNode data;
+}
+Packet;
+
+// struct holding data inside a Packet
+typedef struct DataNode
+{
+	// fields
+	uint8_t type;
+	uint32_t addr;
+	uint32_t data;
+}
+DataNode;
 
 #endif
