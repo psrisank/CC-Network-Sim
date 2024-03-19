03e186dfdd0eaf273c1fc0cb7363a3414d4d4fdf
Ben Owen <ben@bensuniverse.com>
2024-03-18 22:44:07 -0400

    Control node

--- src/control_node.h
+++ src/control_node.h
@@ -1,6 +1,28 @@
 #ifndef __CONTROL_NODE_H__
 #define __CONTROL_NODE_H__
 
+#include "stdint.h"
 
+// definitions for memory node
+#define NUM_PORTS 1
+
+// top-level struct for a memory node
+typedef struct ControlNode
+{
+	uint8_t id;
+	uint32_t time;
+	// array of ports (queues) -> needs to contain a Packet struct type
+	// array of MemoryLines
+}
+ControlNode;
+
+// struct holding data inside a ControlNode
+typedef struct ControlNodeMemoryLine
+{
+	uint8_t address;
+	uint32_t value;
+	// status of all control nodes
+}
+ControlNodeMemoryLine;
 
 #endif
