dabcbfa8fe1f954a70186384d5e6e0f9c01fecf8
Pranav Srisankar <psrisank@purdue.edu>
2024-04-11 13:48:44 -0400

    updated flags in makefile to run in C99 mode

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC = gcc
-CFLAGS = -Wall -Wextra -g
+CFLAGS = -Wall -Wextra -g -std=C99
 
 SRCDIR = src
 BINDIR = bin
--- src/compute_node.h
+++ src/compute_node.h
@@ -7,6 +7,19 @@
 // definitions for memory node
 #define CMP_NUM_BOT_PORTS	1	// should not ever be changed
 #define CMP_QUEUE_SIZE		256	// in packets
+#define CACHE_LINES			4
+
+
+// struct holding data inside a ComputeNode
+typedef struct ComputeNodeMemoryLine
+{
+	uint8_t address;
+	uint32_t value;
+	// status of all compute nodes
+	uint8_t valid;
+	uint8_t dirty;
+}
+ComputeNodeMemoryLine;
 
 // top-level struct for a memory node
 typedef struct ComputeNode
@@ -16,16 +29,9 @@ typedef struct ComputeNode
 	// array of ports (queues) -> needs to contain a Packet struct type
 	// array of MemoryLines
 	Port bot_ports[CMP_NUM_BOT_PORTS];
+	ComputeNodeMemoryLine cache[CACHE_LINES];
 }
 ComputeNode;
 
-// struct holding data inside a ComputeNode
-typedef struct ComputeNodeMemoryLine
-{
-	uint8_t address;
-	uint32_t value;
-	// status of all compute nodes
-}
-ComputeNodeMemoryLine;
 
 #endif
