2412a6d349d486d83bde1fa2cab85f4ab4ee19c0
Pranav Srisankar <psrisank@purdue.edu>
2024-06-25 21:03:33 -0400

    nothing

--- test.bash
+++ test.bash
@@ -1,5 +1,5 @@
 for k in $(seq 0 0.1 1);
 do
     echo "Write ratio: $k" >> out.log
-    cd ~/CC-Network-Sim/artifacts && python3 new_generator.py "$k" 0.8 128 output_trace.csv && cd .. && make -s >> out.log
+    cd ~/CC-Network-Sim/artifacts && python3 new_generator.py "$k" 0.4 128 output_trace.csv && cd .. && make -s >> out.log
 done
